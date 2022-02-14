08b49dd9051b08d4105e88b5e795c713a8f010f8
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-02-14 12:35:18 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function subtract(){
+
+
+}
+
+function multiply(){
+
+
+}
+
+function divide(){
+
+}
+
+function add(){
+
+}
+
+function add (a,b){
+    return a+b;
+}
+
+function subtract (a,b){
+    return a-b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment (n){
+    return ++n;
+}
+
+function decrement (n){
+    return --n;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
